1ee6ed0ceac09d27645d1a3818cbad9b891e73e4
Yarin Miralles <yar@yartechinc.com>
2020-05-26 00:52:29 -0400

    Sec 2 Ch 28

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,13 @@
 const defaultResult = 0;
-
 let currentResult = 0;
+
+// a function does not need to end with a semi-colon
+function add(num1, num2) {
+    const result = num1 + num2;
+    alert('The result is ' + result);
+} 
+
+add(1, 2);
 
 currentResult = currentResult + 10 * 3;
 
