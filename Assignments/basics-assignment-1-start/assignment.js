b3bd9da1ae7090bfbae60fff85bf578af6bf12bb
Yarin Miralles <yar@yartechinc.com>
2020-05-26 00:34:56 -0400

    Sec 2 Ch 26

---
+++ Assignments/basics-assignment-1-start/assignment.js
@@ -0,0 +1,11 @@
+const userValue = 333;
+let result;
+
+result = 18 + userValue;
+result = result - 66;
+result = result * 8;
+result = result / 2;
+alert(`user value is ${userValue}, result is ${result}`);
+alert(`Didn't you hear what I said, fool!`);
+
+
--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,10 @@
+const defaultResult = 0;
+
 let currentResult = 0;
 
 currentResult = currentResult + 10 * 3;
 
-outputResult(currentResult, '');
+let calculationDescription = `(${defaultResult} + 10 * 57)`;
+let errorMessage = 'An error has occured.'
+
+outputResult(currentResult, calculationDescription);
