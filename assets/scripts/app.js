59fd5784df698befed9bb9a33d689d56e2da4a86
Yarin Miralles <yar@yartechinc.com>
2020-06-26 20:24:24 -0400

    Sec 4 Ch 82

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -22,39 +22,52 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
 }
 
 function add() {
-    const enteredNumber = getUserInput();
-    const initialResult = currentResult;
-    currentResult += enteredNumber;
-    createAndWriteOutput('+', initialResult, enteredNumber);
-    writeToLog('ADD', initialResult, enteredNumber, currentResult);
-    console.log(logEntries); 
+    calculateResult('ADD');
 } 
 
 function subtract() {
-    const enteredNumber = getUserInput();
-    const initialResult = currentResult;
-    currentResult -= enteredNumber;
-    createAndWriteOutput('-', initialResult, enteredNumber);
-    writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult);
-    console.log(logEntries);
+    calculateResult('SUBTRACT');
 }
 
 function multiply() {
-    const enteredNumber = getUserInput();
-    const initialResult = currentResult;
-    currentResult *= enteredNumber;
-    createAndWriteOutput('x', initialResult, enteredNumber);
-    writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult);
-    console.log(logEntries);
+    calculateResult('MULTIPLY');
 }
 
 function divide() {
+    calculateResult('DIVIDE');
+}
+function calculateResult(calculationType) {
     const enteredNumber = getUserInput();
+
+    if (
+        calculationType !== 'ADD' &&
+        calculationType !== 'SUBTRACT' &&
+        calculationType !== 'MULTIPLY' &&
+        calculationType !== 'DIVIDE' ||
+        !enteredNumber 
+    ) {
+        return;
+    }
+        
     const initialResult = currentResult;
-    currentResult /= enteredNumber;
-    createAndWriteOutput('/', initialResult, enteredNumber);
-    writeToLog('DIVIDE', initialResult, enteredNumber, currentResult);
-    console.log(logEntries);
+    let mathOperator;
+    if (calculationType === 'ADD') {
+        currentResult += enteredNumber;
+        mathOperator = '+';
+    } else if (calculationType === 'SUBTRACT') {
+        currentResult -= enteredNumber;
+        mathOperator = '-';
+    } else if (calculationType === 'MULTIPLY') {
+        currentResult *= enteredNumber;
+        mathOperator = '*';
+    } else if (calculationType === 'DIVIDE') {
+        currentResult /= enteredNumber;
+        mathOperator = '/';
+    }
+
+    createAndWriteOutput(mathOperator, initialResult, enteredNumber);
+    writeToLog(calculationType, initialResult, enteredNumber, currentResult);
+    console.log(logEntries);  
 }
 
 addBtn.addEventListener('click', add);
